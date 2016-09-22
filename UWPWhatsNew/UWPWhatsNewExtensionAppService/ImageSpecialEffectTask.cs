5e355654694dd05741f4ce48a71719597fdb7114
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2016-09-22 15:34:00 +0200

    Vrai extension de pailletage sur les extensions

--- UWPWhatsNew/UWPWhatsNewExtensionAppService/ImageSpecialEffectTask.cs
+++ UWPWhatsNew/UWPWhatsNewExtensionAppService/ImageSpecialEffectTask.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media.Imaging;
 using Lumia.Imaging;
 using Lumia.Imaging.Adjustments;
 using Lumia.Imaging.Artistic;
+using Lumia.Imaging.Compositing;
 
 namespace UWPWhatsNewExtensionAppService
 {
@@ -48,19 +49,20 @@ namespace UWPWhatsNewExtensionAppService
                     var targetFileToken = message.Request.Message["targetFileToken"].ToString();
                     var targetFile = await SharedStorageAccessManager.RedeemTokenForFileAsync(targetFileToken);
                     var outputFile = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(Guid.NewGuid().ToString("N") + ".jpg");
+                    var foregroundImage = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Paillete.png"));
 
                     //use the blureffect
                     using (var inputStream = await targetFile.OpenReadAsync())
+                    using (var foregroundImageStream = await foregroundImage.OpenReadAsync())
                     {
-                        var blureffect = new PosterizeEffect
-                        {
-                            ColorComponentValueCount = 12
-                        };
+                        var effect = new BlendEffect(new RandomAccessStreamImageSource(inputStream), new RandomAccessStreamImageSource(foregroundImageStream));
                         inputStream.Seek(0);
-                        blureffect.Source = new RandomAccessStreamImageSource(inputStream);
+                        foregroundImageStream.Seek(0);
+                        //effect.Source = new RandomAccessStreamImageSource(inputStream);
+                        //effect.BlendFunction = BlendFunction.Add;
+                        //effect.ForegroundSource = new RandomAccessStreamImageSource(foregroundImageStream);
 
-
-                        using (var jpegRenderer = new JpegRenderer(blureffect))
+                        using (var jpegRenderer = new JpegRenderer(effect))
                         using (var stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
                         {
                             // Jpeg renderer gives the raw buffer that contains the filtered image.
